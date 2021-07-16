160fccf85ffeb52a9d103cdec95c17d50fb8575d
kaurharmanpreet <79268786+kaurharmanpreet@users.noreply.github.com>
2021-07-16 02:07:37 -0700

    final project

---
+++ js/script.js
@@ -0,0 +1,15 @@
+ /*for open nav*/
+ function openNav(){
+	document.getElementById('slide-navbar').style.width="100%";
+	document.getElementById('down-slidemenu').style.display="block";
+	document.getElementById('button').style.display="block";
+}
+
+ /*for close nav*/
+function closeNav(){
+	document.getElementById('slide-navbar').style.width=0;
+	document.getElementById('down-slidemenu').style.display="none";
+	document.getElementById('button').style.display="none";
+}
+
+
